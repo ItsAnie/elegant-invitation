3ebe45e14021934c2631884d274be8b1ad974e62
GM Service <anisahakyan524@gmail.com>
2026-09-22 22:52:23 +0400

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ export default function Hero({ data }) {
   }, []);
 
   return (
-    <section className="relative h-[680px] overflow-hidden sm:h-screen md:h-[820px]">
+    <section className="relative h-[680px] overflow-hidden h-screen">
 
       <img
         src={couple}
