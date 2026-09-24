bbc8c37abc11685f06a02cd940e31e8c838008dc
GM Service <anisahakyan524@gmail.com>
2026-09-25 00:06:24 +0400

    updates

--- src/components/InvitationMessage.jsx
+++ src/components/InvitationMessage.jsx
@@ -8,7 +8,7 @@ const InvitationMessage = () => {
             <img alt="Wedding image" src={engangement} className="rounded-lg" />
             <p className="max-w-[150px] text-center text-xl font-serif italic">Save The Date 20.05.2027</p>
         </div>
-    <section className="text-center px-6 m-[40px]">
+    <section className="text-center px-6">
         
       <h2 className="mb-4 text-[28px] font-armenian uppercase tracking-[0.25em] text-[#555846]">
         Սիրելի՛ հյուրեր
@@ -22,7 +22,7 @@ const InvitationMessage = () => {
       </p>
 
       <p className="mt-8 text-xl font-light font-armenian text-[#555846]">
-        Սիրով՝ Մարի & Արամ
+        Սիրով՝ Մարի & Արման
       </p>
     </section>
     <img alt="Heart divider" src={heartDivider} />
